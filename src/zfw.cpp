34760babd6e1125efac87c219845685f998c3bf3
斐硕硕 <1454615705@qq.com>
2024-11-26 02:05:56 -0800

    bug change

--- src/zfw.cpp
+++ src/zfw.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <cstddef>
 #include "../include/your_header.h"
-#include "../STL/calcCMake.cpp"
+// #include "../STL/calcCMake.cpp"
 // linux 命令：g++ zfw.cpp -o main |   g++ -DDEBUG zfw.cpp -o main2
 int main()
 {
@@ -19,8 +19,8 @@ int main()
   std::cout << "Debug mode is off." << std::endl;
   YourClass yourObject;
   yourObject.yourMethod();
-  int result = add(5, 3);
-  std::cout << "result:" << result << std::endl;
+  // int result = add(5, 3);
+  // std::cout << "result:" << result << std::endl;
 #endif
   return 0;
 }
