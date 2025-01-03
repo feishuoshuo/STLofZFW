a27cd151b93af522ac3fb747791f2668b3ba3b59
feishuoshuo <1454615705@qq.com>
2025-01-02 23:09:20 -0800

    functional adapter of functional.h is ok!

--- src/functional.h
+++ src/functional.h
@@ -8,7 +8,7 @@ namespace zfwstl
 {
   // 定义一元函数的参数型别和返回值型别
   template <class Arg, class Result>
-  struct unarg_function
+  struct unary_function
   {
     typedef Arg argument_type;  // 函数参数型别
     typedef Result result_type; // 传回值型别
@@ -64,7 +64,7 @@ namespace zfwstl
 
   // 否定
   template <class T>
-  struct negate : public unarg_function<T, T>
+  struct negate : public unary_function<T, T>
   {
     T operator()(const T &x) const { return -x; }
   };
@@ -72,7 +72,7 @@ namespace zfwstl
   // ======证同元素================
   // 典型案例：RB-tree所需的KeyOfValue op, set进行实现
   template <class T>
-  struct identity : public unarg_function<T, T>
+  struct identity : public unary_function<T, T>
   {
     const T &operator()(const T &x) const { return x; }
   };
@@ -80,7 +80,7 @@ namespace zfwstl
   // 选择函数(selection function):接受一个pair，传回其第一个元素
   // 典型案例：RB-tree所需的KeyOfValue op, map进行实现
   template <typename Pair>
-  struct select1st : public unarg_function<Pair, typename Pair::first_type>
+  struct select1st : public unary_function<Pair, typename Pair::first_type>
   {
     typename Pair::first_type &operator()(const Pair &pair) const
     {
@@ -90,7 +90,7 @@ namespace zfwstl
 
   // 选择函数(selection function):接受一个pair，传回其第二个元素
   template <typename Pair>
-  struct select2nd : public unarg_function<Pair, typename Pair::second_type>
+  struct select2nd : public unary_function<Pair, typename Pair::second_type>
   {
     typename Pair::second_type &operator()(const Pair &pair) const
     {
@@ -185,13 +185,420 @@ namespace zfwstl
 
   // 逻辑非
   template <class T>
-  struct logical_not : public unarg_function<T, bool>
+  struct logical_not : public unary_function<T, bool>
   {
     bool operator()(const T &x) const { return !x; }
   };
 
-  // =======================哈希函数对象======================
+  /**
+   * function adapter
+   */
+  // ==============对返回值进行逻辑否定not1, not2====================
+  // 表示逻辑负值
+  template <class Predicate>
+  class unary_negate : public unary_function<typename Predicate::argument_type, bool>
+  {
+  protected:
+    Predicate pred; // 内部成员
+  public:
+    explicit unary_negate(const Predicate &x) : pred(x) {}
+    bool operator()(const typename Predicate::argument_type &x) const
+    {
+      return !pred(x); // 将谓词结果加上否定运算
+    }
+  };
 
+  // 辅助函数 (function adapter)
+  template <class Predicate>
+  inline unary_negate<Predicate>
+  not1(const Predicate &pred)
+  {
+    return unary_negate<Predicate>(pred);
+  }
+
+  // 表示逻辑负值
+  template <class Predicate>
+  class binary_negate : public binary_function<typename Predicate::first_argument_type,
+                                               typename Predicate::second_argument_type,
+                                               bool>
+  {
+  protected:
+    Predicate pred; // 内部成员
+  public:
+    explicit binary_negate(const Predicate &x) : pred(x) {}
+    bool operator()(const typename Predicate::first_argument_type &x,
+                    const typename Predicate::second_argument_type &y) const
+    {
+      return !pred(x, y); // 对谓词结果加上否定运算
+    }
+  };
+
+  // 辅助函数 (function adapter)
+  template <class Predicate>
+  inline binary_negate<Predicate>
+  not2(const Predicate &pred)
+  {
+    return binary_negate<Predicate>(pred);
+  }
+
+  // ==============对参数进行绑定bind1st, bind2nd====================
+  //  以下配接器将某个Adaptable Binary function 转换为 Unary Function
+  template <class Operation>
+  class binder1st : public unary_function<typename Operation::second_argument_type,
+                                          typename Operation::result_type>
+  {
+  protected:
+    Operation op;                                  // 内部成员
+    typename Operation::first_argument_type value; // 内部成员
+  public:
+    binder1st(const Operation &x, const typename Operation::first_argument_type &y)
+        : op(x), value(y) {} // 记录表达式和第一参数
+    typename Operation::result_type
+    operator()(const typename Operation::second_argument_type &x) const
+    {
+      return op(value, x); // 实际调用表达式，并将value绑定为第一参数
+    }
+  };
+
+  // 辅助函数 (function adapter)
+  template <class Operation, class Tp>
+  inline binder1st<Operation>
+  bind1st(const Operation &fn, const Tp &x)
+  {
+    typedef typename Operation::first_argument_type _Arg1_type;
+    return binder1st<Operation>(fn, _Arg1_type(x));
+  }
+
+  // 以下配接器将某个Adaptable Binary function 转换为 Unary Function
+  template <class Operation>
+  class binder2nd
+      : public unary_function<typename Operation::first_argument_type,
+                              typename Operation::result_type>
+  {
+  protected:
+    Operation op;
+    typename Operation::second_argument_type value;
+
+  public:
+    binder2nd(const Operation &x,
+              const typename Operation::second_argument_type &y)
+        : op(x), value(y) {} // 记录表达式和第二参数
+    typename Operation::result_type
+    operator()(const typename Operation::first_argument_type &x) const
+    {
+      return op(x, value); // 实际调用表达式，并将value绑定为第二参数
+    }
+  };
+
+  // 辅助函数 (function adapter)
+  template <class Operation, class Tp>
+  inline binder2nd<Operation>
+  bind2nd(const Operation &fn, const Tp &x)
+  {
+    typedef typename Operation::second_argument_type _Arg2_type;
+    return binder2nd<Operation>(fn, _Arg2_type(x));
+  }
+
+  // ==============用于函数合成compose1, compose2====================
+  // unary_compose and binary_compose (extensions, not part of the standard).
+
+  // 已知两个Adaptable Unary Functions f(), g()
+  // 以下配接器用来产生一个h(), h(x) = f(g(x))
+  template <class Operation1, class Operation2>
+  class unary_compose
+      : public unary_function<typename Operation2::argument_type,
+                              typename Operation1::result_type>
+  {
+  protected:
+    Operation1 fn1; // 内部成员
+    Operation2 fn2; // 内部成员
+  public:
+    unary_compose(const Operation1 &x, const Operation2 &y) : fn1(x), fn2(y) {} // 记录两个表达式
+    typename Operation1::result_type
+    operator()(const typename Operation2::argument_type &x) const
+    {
+      return fn1(fn2(x)); // 函数合成
+    }
+  };
+
+  // 辅助函数
+  template <class Operation1, class Operation2>
+  inline unary_compose<Operation1, Operation2>
+  compose1(const Operation1 &fn1, const Operation2 &fn2)
+  {
+    return unary_compose<Operation1, Operation2>(fn1, fn2);
+  }
+
+  // 已知一个Adaptable Binary Functions f 和
+  // 两个Adaptable Unary Functions g1(), g2()
+  // 以下配接器用来产生一个h(), h(x) = f(g1(x), g2(x))
+  template <class Operation1, class Operation2, class Operation3>
+  class binary_compose : public unary_function<typename Operation2::argument_type,
+                                               typename Operation1::result_type>
+  {
+  protected:
+    // 内部成员
+    Operation1 fn1;
+    Operation2 fn2;
+    Operation3 fn3;
+
+  public:
+    binary_compose(const Operation1 &x, const Operation2 &y, const Operation3 &z)
+        : fn1(x), fn2(y), fn3(z) {} // 记录表达式
+    typename Operation1::result_type
+    operator()(const typename Operation2::argument_type &x) const
+    {
+      return fn1(fn2(x), fn3(x)); // 函数合成
+    }
+  };
+
+  // 辅助函数
+  template <class Operation1, class Operation2, class Operation3>
+  inline binary_compose<Operation1, Operation2, Operation3>
+  compose2(const Operation1 &fn1, const Operation2 &fn2, const Operation3 &fn3)
+  {
+    return binary_compose<Operation1, Operation2, Operation3>(fn1, fn2, fn3);
+  }
+  // ==============用于函数指针ptr_fun====================
+  // 这种配接器adapter使我们能将一般函数当作仿函数使用
+  // 以下配接器将一个一元函数指针包装起来
+  // 使用仿函数时就调用函数指针
+  template <class Arg, class Result>
+  class pointer_to_unary_function : public unary_function<Arg, Result>
+  {
+  protected:
+    Result (*ptr)(Arg); // 内部成员，函数指针
+  public:
+    pointer_to_unary_function() {}
+    // 记录函数指针
+    explicit pointer_to_unary_function(Result (*x)(Arg)) : ptr(x) {}
+
+    // 通过函数指针执行函数
+    Result operator()(Arg x) const { return ptr(x); }
+  };
+
+  // 辅助函数
+  template <class Arg, class Result>
+  inline pointer_to_unary_function<Arg, Result> ptr_fun(Result (*x)(Arg))
+  {
+    return pointer_to_unary_function<Arg, Result>(x);
+  }
+
+  // 以下配接器将一个二元函数指针包装起来
+  // 使用仿函数时就调用函数指针
+  template <class Arg1, class Arg2, class Result>
+  class pointer_to_binary_function : public binary_function<Arg1, Arg2, Result>
+  {
+  protected:
+    Result (*ptr)(Arg1, Arg2); // 内部成员，函数指针
+  public:
+    pointer_to_binary_function() {}
+    // 记录函数指针
+    explicit pointer_to_binary_function(Result (*x)(Arg1, Arg2))
+        : ptr(x) {}
+
+    // 通过函数指针执行函数
+    Result operator()(Arg1 x, Arg2 y) const
+    {
+      return ptr(x, y);
+    }
+  };
+
+  // 辅助函数
+  template <class Arg1, class Arg2, class Result>
+  inline pointer_to_binary_function<Arg1, Arg2, Result>
+  ptr_fun(Result (*x)(Arg1, Arg2))
+  {
+    return pointer_to_binary_function<Arg1, Arg2, Result>(x);
+  }
+  // ==============用于成员函数指针mem_fun, mem_fun_ref====================
+  // NOTE: 一定要以配接器mem_fun修饰成员函数，才能被算法for_each接受
+  // NOTE： STL容器只支持“实值语意”(value semantic)，不支持“引用语意”(rederence semantics)
+  // 8 = 2^3个函数对象
+  // 1. 无参 vs 有一个参数
+  // 2. 指针调用 vs 引用调用
+  // 3. const 成员函数 vs non-const 成员函数
+
+  // 无参，指针调用，non-const
+  template <class Ret, class Tp>
+  class mem_fun_t : public unary_function<Tp *, Ret>
+  {
+  public:
+    // 记录
+    explicit mem_fun_t(Ret (Tp::*pf)()) : f(pf) {}
+    // 调用
+    Ret operator()(Tp *p) const { return (p->*f)(); }
+
+  private:
+    Ret (Tp::*f)(); // 内部成员，指向成员函数指针
+  };
+
+  // 无参，指针调用，const
+  template <class Ret, class Tp>
+  class const_mem_fun_t : public unary_function<const Tp *, Ret>
+  {
+  public:
+    explicit const_mem_fun_t(Ret (Tp::*pf)() const) : f(pf) {}
+    Ret operator()(const Tp *p) const { return (p->*f)(); }
+
+  private:
+    Ret (Tp::*f)() const;
+  };
+
+  // 无参，引用调用，non-const
+  template <class Ret, class Tp>
+  class mem_fun_ref_t : public unary_function<Tp, Ret>
+  {
+  public:
+    explicit mem_fun_ref_t(Ret (Tp::*pf)()) : f(pf) {}
+    Ret operator()(Tp &r) const { return (r.*f)(); }
+
+  private:
+    Ret (Tp::*f)();
+  };
+
+  // 无参，引用调用，const
+  template <class Ret, class Tp>
+  class const_mem_fun_ref_t : public unary_function<Tp, Ret>
+  {
+  public:
+    explicit const_mem_fun_ref_t(Ret (Tp::*pf)() const) : f(pf) {}
+    Ret operator()(const Tp &r) const { return (r.*f)(); }
+
+  private:
+    Ret (Tp::*f)() const;
+  };
+
+  // 一个参数，指针调用，non-const
+  template <class Ret, class Tp, class Arg>
+  class mem_fun1_t : public binary_function<Tp *, Arg, Ret>
+  {
+  public:
+    explicit mem_fun1_t(Ret (Tp::*pf)(Arg)) : f(pf) {}
+    Ret operator()(Tp *p, Arg x) const { return (p->*f)(x); }
+
+  private:
+    Ret (Tp::*f)(Arg);
+  };
+
+  // 一个参数，指针调用，const
+  template <class Ret, class Tp, class Arg>
+  class const_mem_fun1_t : public binary_function<const Tp *, Arg, Ret>
+  {
+  public:
+    explicit const_mem_fun1_t(Ret (Tp::*pf)(Arg) const) : f(pf) {}
+    Ret operator()(const Tp *p, Arg x) const
+    {
+      return (p->*f)(x);
+    }
+
+  private:
+    Ret (Tp::*f)(Arg) const;
+  };
+
+  // 一个参数，引用调用，non-const
+  template <class Ret, class Tp, class Arg>
+  class mem_fun1_ref_t : public binary_function<Tp, Arg, Ret>
+  {
+  public:
+    explicit mem_fun1_ref_t(Ret (Tp::*pf)(Arg)) : f(pf) {}
+    Ret operator()(Tp &r, Arg x) const { return (r.*f)(x); }
+
+  private:
+    Ret (Tp::*f)(Arg); // 内部成员，pointer to member function
+  };
+
+  // 一个参数，引用调用，const
+  template <class Ret, class Tp, class Arg>
+  class const_mem_fun1_ref_t : public binary_function<Tp, Arg, Ret>
+  {
+  public:
+    explicit const_mem_fun1_ref_t(Ret (Tp::*pf)(Arg) const) : f(pf) {}
+    Ret operator()(const Tp &r, Arg x) const { return (r.*f)(x); }
+
+  private:
+    Ret (Tp::*f)(Arg) const;
+  };
+
+  // 辅助函数们
+  template <class Ret, class Tp>
+  inline mem_fun_t<Ret, Tp> mem_fun(Ret (Tp::*f)())
+  {
+    return mem_fun_t<Ret, Tp>(f);
+  }
+
+  template <class Ret, class Tp>
+  inline const_mem_fun_t<Ret, Tp> mem_fun(Ret (Tp::*f)() const)
+  {
+    return const_mem_fun_t<Ret, Tp>(f);
+  }
+
+  template <class Ret, class Tp>
+  inline mem_fun_ref_t<Ret, Tp> mem_fun_ref(Ret (Tp::*f)())
+  {
+    return mem_fun_ref_t<Ret, Tp>(f);
+  }
+
+  template <class Ret, class Tp>
+  inline const_mem_fun_ref_t<Ret, Tp> mem_fun_ref(Ret (Tp::*f)() const)
+  {
+    return const_mem_fun_ref_t<Ret, Tp>(f);
+  }
+
+  // 以下可以用函数重载
+  template <class Ret, class Tp, class Arg>
+  inline mem_fun1_t<Ret, Tp, Arg> mem_fun(Ret (Tp::*f)(Arg))
+  {
+    return mem_fun1_t<Ret, Tp, Arg>(f);
+  }
+
+  template <class Ret, class Tp, class Arg>
+  inline const_mem_fun1_t<Ret, Tp, Arg> mem_fun(Ret (Tp::*f)(Arg) const)
+  {
+    return const_mem_fun1_t<Ret, Tp, Arg>(f);
+  }
+
+  template <class Ret, class Tp, class Arg>
+  inline mem_fun1_ref_t<Ret, Tp, Arg> mem_fun_ref(Ret (Tp::*f)(Arg))
+  {
+    return mem_fun1_ref_t<Ret, Tp, Arg>(f);
+  }
+
+  template <class Ret, class Tp, class Arg>
+  inline const_mem_fun1_ref_t<Ret, Tp, Arg>
+  mem_fun_ref(Ret (Tp::*f)(Arg) const)
+  {
+    return const_mem_fun1_ref_t<Ret, Tp, Arg>(f);
+  }
+
+  template <class Ret, class Tp, class Arg>
+  inline mem_fun1_t<Ret, Tp, Arg> mem_fun1(Ret (Tp::*f)(Arg))
+  {
+    return mem_fun1_t<Ret, Tp, Arg>(f);
+  }
+
+  template <class Ret, class Tp, class Arg>
+  inline const_mem_fun1_t<Ret, Tp, Arg> mem_fun1(Ret (Tp::*f)(Arg) const)
+  {
+    return const_mem_fun1_t<Ret, Tp, Arg>(f);
+  }
+
+  template <class Ret, class Tp, class Arg>
+  inline mem_fun1_ref_t<Ret, Tp, Arg> mem_fun1_ref(Ret (Tp::*f)(Arg))
+  {
+    return mem_fun1_ref_t<Ret, Tp, Arg>(f);
+  }
+
+  template <class Ret, class Tp, class Arg>
+  inline const_mem_fun1_ref_t<Ret, Tp, Arg>
+  mem_fun1_ref(Ret (Tp::*f)(Arg) const)
+  {
+    return const_mem_fun1_ref_t<Ret, Tp, Arg>(f);
+  }
+
+  /**
+   * 哈希函数对象
+   */
   // 对于大部分类型，hash function 什么都不做
   template <class Key>
   struct hash
--- test/test_adaptor_function.cpp
+++ test/test_adaptor_function.cpp
@@ -1,6 +1,34 @@
 #include "../src/iterator.h" //for ostream_iterator, istream_iterator
 #include "../STL/vector.h"
+#include "../src/algorithms/algorithm.h"
+#include "../src/functional.h" //for multiplies, plus, less
 #include <iostream>
+
+void print(int i)
+{
+  cout << i << " ";
+}
+
+class Int
+{
+public:
+  // explicit 只能修改只有一个参数的类构造函数，
+  // 或是除了第一个参数外其他参数都有默认值的情况
+  // 表明该构造函数是显示的而非隐式的
+  // 作用是禁止类构造函数的隐式自动转换
+  // implicit 表示隐式，类构造函数默认声明为隐式
+  // google c++规范与effective c++都推荐使用explicit声明
+  explicit Int(int i) : m_i(i) {}
+
+  void print1() const
+  {
+    cout << "[" << m_i << "]";
+  }
+
+private:
+  int m_i;
+};
+
 int main()
 {
   /**
@@ -14,34 +42,34 @@ int main()
 
   // 找出不小于12的元素个数
   std::cout << zfwstl::count_if(iv.begin(), iv.end(),
-                                not1(bind2nd(less<int>(), 12)));
+                                zfwstl::not1(zfwstl::bind2nd(zfwstl::less<int>(), 12)));
   std::cout << std::endl;
 
   // 令每个元素v执行(v+2)*3然后输往outite
   transform(iv.begin(), iv.end(), outite,
-            compose1(bind2nd(multiplies<int>(), 3),
-                     bind2nd(plus<int>(), 2)));
+            zfwstl::compose1(zfwstl::bind2nd(zfwstl::multiplies<int>(), 3),
+                             zfwstl::bind2nd(zfwstl::plus<int>(), 2)));
   std::cout << std::endl;
 
   // 将所有元素拷贝到outite
-  copy(iv.begin(), iv.end(), outite);
+  zfwstl::copy(iv.begin(), iv.end(), outite);
   std::cout << std::endl;
   // 1. 使用函数指针搭配stl算法
-  for_each(iv.begin(), iv.end(), print);
+  zfwstl::for_each(iv.begin(), iv.end(), print);
   std::cout << std::endl;
 
   // 2. 以修饰过的一般函数搭配stl算法
-  for_each(iv.begin(), iv.end(), ptr_fun(print));
+  zfwstl::for_each(iv.begin(), iv.end(), zfwstl::ptr_fun(print));
   std::cout << std::endl;
 
   Int t1(3), t2(7), t3(20), t4(14), t5(68);
-  vector<Int> Iv;
+  zfwstl::vector<Int> Iv;
   Iv.push_back(t1);
   Iv.push_back(t2);
   Iv.push_back(t3);
   Iv.push_back(t4);
   Iv.push_back(t5);
   // 3. 以修饰过的成员函数搭配stl算法
-  for_each(Iv.begin(), Iv.end(), mem_fun_ref(&Int::print1));
+  zfwstl::for_each(Iv.begin(), Iv.end(), zfwstl::mem_fun_ref(&Int::print1));
   return 0;
 }
